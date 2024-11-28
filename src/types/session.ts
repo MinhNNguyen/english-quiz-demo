d6370e9fba92d3a7ab81d21f9e31ca91be8e68cc
Minh Nguyen <robertminhnnguyen@gmail.com>
2024-11-28 07:19:15 +0700

    feat: update mock data

--- src/types/session.ts
+++ src/types/session.ts
@@ -7,7 +7,8 @@ export enum SessionState {
 export const SessionCategoryMap = {
   1: 'Basic English Vocab I',
   2: 'Basic English Vocab II',
-  3: 'Intermediate English Vocab I'
+  3: 'Intermediate English Vocab I',
+  4: 'Geographic English Vocab I'
 };
 
 export interface SessionI {
